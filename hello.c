13dfbdef1a7f22955aecfe332778a8604cf0dd41
ym <2521068558@qq.com>
2019-07-07 00:58:12 +0800

    pullrequest

--- hello.c
+++ hello.c
@@ -2,6 +2,9 @@
 
 int main()
 {
+    //notion: in the main function,it is necessary to describe the client's function however simple the code is.
+
+    //print the sentence "hello world".
     printf("Hello world!\n");
     return 0;
 }
