3f71dac4afde3aabbb142a2665f68921550db5d3
ym <2521068558@qq.com>
2019-07-07 16:33:25 +0800

    new request

--- hello.c
+++ hello.c
@@ -2,9 +2,8 @@
 
 int main()
 {
-    //notion: in the main function,it is necessary to describe the client's function however simple the code is.
 
-    //print the sentence "hello world".
+    //the client which prints the sentence "hello world".
     printf("Hello world!\n");
     return 0;
 }
